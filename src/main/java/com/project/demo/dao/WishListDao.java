50bcd3615b9cdfdb2ac8f3812060ed76baf65f0a
TaeMin <xoals3094@naver.com>
2023-11-26 23:55:21 +0900

    feat: service 구현체 및 dao 구현

---
+++ src/main/java/com/project/demo/dao/WishListDao.java
@@ -0,0 +1,10 @@
+package com.project.demo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WishListDao {
+    public void appendProductId(int productId){
+
+    }
+}
