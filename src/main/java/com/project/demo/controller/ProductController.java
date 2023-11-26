bfbc11e720986c2207ff15137d7926466e8475f3
TaeMin <xoals3094@naver.com>
2023-11-26 22:59:48 +0900

    feat: 상품 조회 기능 구현

---
+++ src/main/java/com/project/demo/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.project.demo.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.demo.domain.User;
+import com.project.demo.dto.Product;
+import com.project.demo.dto.Products;
+import com.project.demo.service.IProductService;
+
+@RestController
+class ProductController {
+    private final IProductService productService;
+
+    public ProductController(IProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping("products")
+    public Products getProducts() {
+        return new Products(this.productService.getProducts());
+    }
+
+    @GetMapping("products/{productId}")
+    public Product getProductDetail(@PathVariable("productId") int productId) {
+        return productService.getProduct(productId);
+    }
+}
