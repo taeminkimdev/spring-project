eef10eb98ef64eb65657886af1da78bf41b0deb4
SonYeonggi <147638346+SonYeonggi@users.noreply.github.com>
2023-12-03 17:38:12 +0900

    Create SuccessResponseDto.java

---
+++ src/main/java/com/project/demo/board/dto/SuccessResponseDto.java
@@ -0,0 +1,10 @@
+package com.api.board.dto;
+
+import lombok.Getter;
+
+@Getter 
+public class SuccessResponseDto {
+    private boolean success;
+
+    public SuccessResponseDto(boolean success) {this.success = success;}
+}
